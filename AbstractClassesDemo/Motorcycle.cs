348a35d4a3859fbd4aa3d354c27ab3d7c39fd733
unknown <jayapatel864@gmail.com>
2020-07-06 13:31:20 -0500

    finished 7 6

---
+++ AbstractClassesDemo/Motorcycle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassesDemo
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle
+       { 
+
+     
+          public bool HasSideCart { get; set; } = false;
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcycle is in drive");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } } 
---
+++ AbstractClassesDemo/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AbstractClassesDemo
+
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "default make";
+        public string Model { get; set; } = "default model";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This{GetType().Name} is virtually in drive");
+
+        }
+
+
+
+
+
+    }
+
+}
